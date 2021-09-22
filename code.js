521d788c6cc71827a1dee4d73db310ef93443dd3
cenriquevs <91174500+cenriquevs@users.noreply.github.com>
2021-09-21 22:56:04 -0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,14 @@
+(function () {
+    var lista = document.getElementById("lista"),
+        tareaInput = document.getElementById("entrada"),
+        btnPost = document.getElementById("boton");
+
+//funciones
+var agregarTarea = function(){
+
+//eventos 
+    //agregar post
+    btnPost.addEventListener("click", agregarTarea);
+
+
+}())
